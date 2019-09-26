339a54995baf3b8eb85b84f099187c1033fe7a31
Harlan Colclough <35181393+hcolclou@users.noreply.github.com>
2019-09-26 00:07:55 -0700

    undoing changes to project_path

--- template/src/project_path.hpp
+++ template/src/project_path.hpp
@@ -2,4 +2,4 @@
 
 // Please don't change the content of this header
 
-#define PROJECT_SOURCE_DIR "C:/Users/Harlan Colclough/repos/echos_in_the_dark/template/"
+#define PROJECT_SOURCE_DIR "C:/Users/luciano/Documents/template/"
