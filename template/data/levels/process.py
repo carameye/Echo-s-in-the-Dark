01d4a9d6bb58ebd109032d9ad205a12130abe80d
Harlan Colclough <hccol@students.cs.ubc.ca>
2019-10-25 21:34:59 -0700

    shadow maps are being properly generated for the level torches

--- template/data/levels/process.py
+++ template/data/levels/process.py
@@ -2,7 +2,53 @@ from os import listdir
 from os.path import dirname, abspath, isfile, join
 import json
 
+from PIL import Image
+from PIL import ImageFilter
+
+from math import sqrt, copysign
+
+def line_len(x1, y1, x2, y2):
+    l = (x2 - x1)**2
+    l += (y2 - y1)**2
+    return sqrt(l)
+
+def inlight(x1, y1, x2, y2, bricks):
+    if x1 is x2 and y1 is y2:
+        return True
+
+    sign = lambda x: copysign(1, x)
+
+    dy = y2 - y1
+    dx = x2 - x1
+
+    x = x1
+    y = y1
+
+    if abs(dy) > abs(dx):
+        if dy is 0:
+            dx = 1
+        else:
+            dx = dx / abs(dy)
+            dy = sign(dy)
+    else:
+        if dx is 0:
+            dy = 1
+        else:
+            dy = dy / abs(dx)
+            dx = sign(dx)
+
+    while line_len(x1, y1, x, y) < line_len(x1, y1, x2, y2):
+
+        if bricks[x, y][0] == 0:
+            return False
+
+        x += 10 * dx
+        y += 10 * dy
+
+    return True
+
 def convert(filepath):
+    print("Converting " + filepath)
     file = open(filepath)
     if file.mode == "r":
         # setup
@@ -96,7 +142,10 @@ def convert(filepath):
                     torches.append(obj)
                     continue
 
-        j["size"] = { "width": sizex + 1, "height": sizey + 1}
+        sizex += 1
+        sizey += 1
+
+        j["size"] = { "width": sizex, "height": sizey}
         j["doors"] = doors
         j["signs"] = signs
         j["ghosts"] = ghosts
@@ -106,12 +155,59 @@ def convert(filepath):
         dirpath = dirname(abspath(__file__))
         filename = filepath.replace(dirpath, "")
         filename = filename.replace("\\", "")
-        filename = filename.replace(".txt", ".json")
-        writepath = join(dirpath, "json/"+filename)
+        filename = filename.replace(".txt", "")
+        writepath = join(dirpath, "json", filename + ".json")
 
         file = open(writepath, "w+")
         file.write(json.dumps(j))
         file.close()
+
+        sizex *= 64
+        sizey *= 64
+
+        print("    Loading bricks")
+
+        brickimage = Image.new("RGB", (sizex, sizey), color = (255, 255, 255))
+        brickpixels = brickimage.load()
+
+        for b in bricks:
+            startx = 64 * b["pos"]["x"]
+            starty = 64 * b["pos"]["y"]
+            for i in range(startx, startx + 64):
+                for j in range(starty, starty + 64):
+                    brickpixels[i, j] = (0, 0, 0)
+
+        brickimage.save(join(dirpath, "shadow", filename + "_brickmap.png"))
+
+        print("    Loading shadows")
+
+        shadowimage = Image.new("RGB", (sizex, sizey), color = (0, 0, 0))
+        shadowpixels = shadowimage.load()
+
+        t_num = 1
+        for t in torches:
+            print("        Assessing torch " + str(t_num) + " out of " + str(len(torches)))
+            t_num += 1
+            x = 64 * t["pos"]["x"]
+            y = 64 * t["pos"]["y"]
+            for i in range(max(0, x - 384), min(sizex, x + 384)):
+                for j in range(max(0, y - 384), min(sizey, y + 384)):
+                    if shadowpixels[i, j] is (255, 255, 255):
+                        continue;
+                    if inlight(i, j, x, y, brickpixels):
+                        shadowpixels[i, j] = (255, 255, 255)
+
+        print("    Refining shadows")
+
+        shadowimage = shadowimage.filter(ImageFilter.GaussianBlur(16))
+
+        for i in range(0, sizex):
+            for j in range(0, sizey):
+                val = min(2 * shadowpixels[i, j][0], 255)
+                shadowpixels[i, j] = (val, val, val)
+
+        print("    Saving images")
+        shadowimage.save(join(dirpath, "shadow", filename + "_shadowmap.png"))
 
 def convertall():
     path = dirname(abspath(__file__))
